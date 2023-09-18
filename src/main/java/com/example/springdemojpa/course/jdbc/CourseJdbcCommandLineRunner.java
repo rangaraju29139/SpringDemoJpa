1e0da0e129a616d8d591ef71a34427b729d5606d
rangaraju penmetsa <rangaraju29139@gmail.com>
2023-09-18 22:06:28 +0530

    added the insert and delete query using commandLine runner

--- src/main/java/com/example/springdemojpa/course/jdbc/CourseJdbcCommandLineRunner.java
+++ src/main/java/com/example/springdemojpa/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -1,5 +1,6 @@
 package com.example.springdemojpa.course.jdbc;
 
+import com.example.springdemojpa.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,8 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
     private CourseJdbcRepository repository;
     @Override
     public void run(String... args) throws Exception {
-       repository.insert();
+       repository.insert(new Course(1,"learn Aws Now","in28Minutes"));
+        repository.insert(new Course(2,"learn Aws Now Now","in28Minutes"));
+        repository.deleteById(1);
     }
 }
--- src/main/java/com/example/springdemojpa/course/jdbc/CourseJdbcRepository.java
+++ src/main/java/com/example/springdemojpa/course/jdbc/CourseJdbcRepository.java
@@ -1,5 +1,6 @@
 package com.example.springdemojpa.course.jdbc;
 
+import com.example.springdemojpa.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -14,12 +15,21 @@ public class CourseJdbcRepository {
 
     String insertQuery =
             """
-            insert into course(id,name,author) VALUES (1,'Learn Aws','aws academy');
+            insert into course(id,name,author) VALUES (?,?,?);
             
             """;
 
-    public void insert(){
-        springJdbcTemplate.update(insertQuery);
+    String deleteQuery =
+            """
+                    delete from course where id = ?;
+                    """;
+
+    public void insert(Course course){
+        springJdbcTemplate.update(insertQuery,course.getId(),course.getName(),course.getAuthor());
+    }
+
+    public void deleteById(long id){
+        springJdbcTemplate.update(deleteQuery,id);
     }
 
 }
